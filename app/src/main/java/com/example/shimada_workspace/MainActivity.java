33da5c35dbe630cd1d7bb88094b794c6839a8987
k.shimada <k.shimada@mediajoho.com>
2020-05-15 22:01:10 +0900

    ビルドエラー解消

--- app/src/main/java/com/example/shimada_workspace/MainActivity.java
+++ app/src/main/java/com/example/shimada_workspace/MainActivity.java
@@ -21,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
                 Log.d("button","ボタン押されたよ");Log.d("button","ボタン押されたよ");
                 Log.d("button","ボタン押されたよ");Log.d("button","ボタン押されたよ");
                 Log.d("button","ボタン押されたよ");Log.d("button","ボタン押されたよ");
+                Log.d("button","ボタン押されたよ");Log.d("button","ボタン押されたよ");
+
 
             }
 
